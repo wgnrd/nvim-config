ef33424ae43bf61b0ff800af505a2e9e6be55d50
wgnrd <hallo@wagnerdominik.at>
2024-11-27 15:19:41 +0100

    split plugin configs in different files

--- init.lua
+++ init.lua
@@ -3,8 +3,10 @@ require("wgnrd.remap")
 
 vim.opt.relativenumber = true
 vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 vim.g.indent_guides_enable_on_vim_startup = 1
 vim.g.indnet_guides_start_level = 2
 vim.g.indent_guides_guide_size = 1
-vim.cmd([[Neotree]])
 vim.cmd([[colorscheme dracula]])
+
