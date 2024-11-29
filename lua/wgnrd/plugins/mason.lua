69f7497192fcefd9875cbcb52fbed22f5373e399
wgnrd <hallo@wagnerdominik.at>
2024-11-29 08:15:13 +0100

    remove lazy-lock

--- lua/wgnrd/plugins/mason.lua
+++ lua/wgnrd/plugins/mason.lua
@@ -1,5 +1,28 @@
 return {
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/nvim-cmp" },
+	"williamboman/mason.nvim",
+	{ "williamboman/mason-lspconfig.nvim" },
+	"neovim/nvim-lspconfig",
+
+	config = function()
+		local mason = require("mason")
+		local lspconfig = require("lspconfig")
+		require("mason-lspconfig").setup({
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+			},
+		})
+
+		mason.setup()
+
+		lspconfig.tsserver.setup({
+			on_attach = function(client)
+				client.resolved_capabilities.document_formatting = false
+			end,
+		})
+	end,
 }
+
